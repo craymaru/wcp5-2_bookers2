99b771ac67e75f829be816d76fd1d09346fd0a8a
Cray <craymaru@gmail.com>
2019-12-25 19:08:21 +0900

    [Update] Book controllers.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,42 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    # Another Syntax
+    # book = current_user.book.new(book_params_params)
+    book.save
+    redirect_to books_path
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
