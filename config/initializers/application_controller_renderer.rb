fbdc140bf0d25ba32237879a06adaf2c6a7a6c1d
Cray <craymaru@gmail.com>
2019-12-26 15:23:22 +0900

    [Fix] User Img Update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# Adds: Refile Key
+Refile.secret_key = '0caf53f224fb9971fe6fb965184e5002bf95ba4f48e5e08c7810b5cb5506825d77099bc18b14ad2529b57145b732bc04667690205ec6eb869241338f0f0d4347'
