079bd52ed0b232d1565848b3b820deb4252d2c70
Cray <craymaru@gmail.com>
2019-12-26 15:09:57 +0900

    [Update] Adapt templates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user # left contents
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,29 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new # left contents
+    @user = current_user # left contents
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new # left content
   end
 
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+end
 end
