971cc0f071b7299d90f793037a1446931f655019
Cray <craymaru@gmail.com>
2019-12-26 14:32:25 +0900

    [Update] controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
   devise_for :users
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
